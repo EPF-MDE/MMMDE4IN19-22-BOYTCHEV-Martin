ea0250103848016128549b75fdb5705b745abb13
Martin Boytchev <martinboytchev@MacBook-Air-de-Martin.local>
2024-03-06 11:21:59 +0100

    slide 22

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World & Alien?!')
 })
 
+app.get('/students', function (req, res) {
+  const studentsInfo = [
+    { name: "Eric Burel", school: "EPF" },
+    { name: "Harry Potter", school: "Poudlard" }
+  ];
+  res.send(studentsInfo);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
 })
