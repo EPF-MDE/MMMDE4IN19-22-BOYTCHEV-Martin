5dbe54bb0df6dd869c4aa985aad632e8464a5ed0
Martin Boytchev <martinboytchev@MacBook-Air-de-Martin.local>
2024-03-06 12:23:42 +0100

    bonus marche pas

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require("fs");
 
-app.get('/', (req, res) => {
-  res.send('Hello World & Alien?!')
-})
+app.get('/', (req, res) => res.send('Hello World & Alien?!'));
 
-app.get('/students', function (req, res) {
-  const studentsInfo = [
-    { name: "Eric Burel", school: "EPF" },
-    { name: "Harry Potter", school: "Poudlard" }
-  ];
-  res.send(studentsInfo);
+app.get('/students', (req, res) => {
+  const rowSeparator = "\n";
+  const cellSeparator = ",";
+  fs.readFile('./students.csv', 'utf8', (err, data) => {
+    console.log("Contenu de students.csv :");
+    console.log(data);
+
+    const rows = data.split(rowSeparator);
+    const[headerRow, ...contentRows]=rows ;
+    const header = headerRow.split(cellSeparator);
+
+    const students = contentRows.map(row => {
+      const cells = row.split(cellSeparator);
+      const student ={
+        [header[0]]: cells[0],
+        [header[1]]: cells[1],
+      };
+      return student
+    });
+    res.send(students);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
+app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`)); 
