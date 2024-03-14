090bee2661aaaa0fbf0cf9b9b20e011643c4bb01
Martin Boytchev <martinboytchev@MacBook-Air-de-Martin.local>
2024-03-14 15:10:27 +0100

    page 14 TP3

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require("fs");
+const path = require("path");
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "./views/home.html")));
 app.get('/', (req, res) => res.send('Hello World & Alien?!'));
 
 app.use(express.json());
 
-app.post('/students/create', (req, res) => {
+app.post('/api/students/create', (req, res) => {
   const csvLine =`\n${req.body.name},${req.body.school}`
   console.log(csvLine);
   res.send("Student created");
@@ -16,7 +18,7 @@ app.post('/students/create', (req, res) => {
   });
 });
 
-app.get('/students', (req, res) => {
+app.get('/api/students', (req, res) => {
   const rowSeparator = "\r\n";
   const cellSeparator = ",";
   fs.readFile('./students.csv', 'utf8', (err, data) => {
