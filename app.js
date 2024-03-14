7711c31fd9e0414d613e5f5f11b419a72fbc0abb
Martin Boytchev <martinboytchev@MacBook-Air-de-Martin.local>
2024-03-14 20:57:49 +0100

    slide 34

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const fs = require("fs");
 const path = require("path");
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.set("views", "./views"); 
 app.set("view engine", "ejs");
@@ -12,34 +13,29 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./views/home.html"
 //app.get('/', (req, res) => res.send('Hello World & Alien?!'));
 
 app.get("/students", (req, res) => {
-  const rowSeparator = "\r\n";
+  const rowSeparator = "\n";
   const cellSeparator = ",";
-  fs.readFile("students.csv", 'utf-8', (err, data) => {
-    if (err) {
-      console.error("Erreur lors de la lecture du fichier students.csv :", err);
-      res.status(500).send("Erreur lors de la lecture du fichier students.csv");
-      return;
-    }
+  fs.readFile("./students.csv", 'utf-8', (err, data) => {
     console.log("Contenu de students.csv :");
     console.log(data);
 
     const rows = data.split(rowSeparator);
     const [headerRow, ...contentRows] = rows;
     const header = headerRow.split(cellSeparator);
-  
+
     const students = contentRows.map((row) => {
       const cells = row.split(cellSeparator);
       const student = {
         [header[0]]: cells[0],
         [header[1]]: cells[1],
       };
-      console.log(student)
+      console.log(student);
       return student;
     });
-    res.render("students", { students, 
-    });
+    res.render("students", { students });
   });
 });
+
 
 app.get("/students/create", (req, res) => {
   res.render("create-student");
@@ -48,9 +44,13 @@ app.get("/students/create", (req, res) => {
 app.post("/students/create", (req, res) => {
   console.log(req.body);
   const name = req.body.name;
-  const school = req.body.school;
-  console.log("New student created:", name, school);
-  res.redirect("/students/create?created=1"); 
+  const school = req.body.school; 
+  const csvLine =`\n${name},${school}`
+  console.log(csvLine);
+  fs.writeFile("./students.csv", csvLine, { flag: 'a' }, (err) => {
+    console.log("Added!");
+  });
+  res.redirect("/students/create?created=1");
 });
 
 app.use(express.json());
